13e1c5779fe7585d5553ff5bb0f94d6882ca23ba
Sahil Rana <sahilrana27582@gmail.com>
2025-06-25 20:46:02 +0530

    Task Queue | Model Ready, Queue Ready, Redis Ready

---
+++ internal/queue/queue.go
@@ -0,0 +1,73 @@
+package queue
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/sahilrana27582/go-task-queue/models"
+)
+
+const RedisTaskQueueKey = "task_queue"
+
+type RedisQueue struct {
+	redisClient *redis.Client
+	ctx         context.Context
+}
+
+func NewRedisQueue(addr string) *RedisQueue {
+	client := redis.NewClient(&redis.Options{
+		Addr: addr,
+	})
+	return &RedisQueue{
+		redisClient: client,
+		ctx:         context.Background(),
+	}
+}
+
+func (rq *RedisQueue) Enqueue(task *models.Task) error {
+	data, err := json.Marshal(task)
+	if err != nil {
+		return err
+	}
+	return rq.redisClient.RPush(rq.ctx, RedisTaskQueueKey, data).Err()
+}
+
+func (rq *RedisQueue) Dequeue(timeout time.Duration) (*models.Task, error) {
+	result, err := rq.redisClient.BLPop(rq.ctx, timeout, RedisTaskQueueKey).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	if len(result) < 2 {
+		return nil, errors.New("invalid task data")
+	}
+
+	var task models.Task
+	if err := json.Unmarshal([]byte(result[1]), &task); err != nil {
+		return nil, err
+	}
+	task.Status = models.StatusInProgress
+	if err := rq.redisClient.LRem(rq.ctx, RedisTaskQueueKey, 0, result[1]).Err(); err != nil {
+		return nil, err
+	}
+
+	return &task, nil
+}
+
+func (rq *RedisQueue) Length() (int64, error) {
+	return rq.redisClient.LLen(rq.ctx, RedisTaskQueueKey).Result()
+}
+
+func (rq *RedisQueue) Ping() (string, error) {
+	status, err := rq.redisClient.Ping(rq.ctx).Result()
+	if err != nil {
+		return "", err
+	}
+	return status, nil
+}
---
+++ models/task.go
@@ -0,0 +1,47 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type Status string
+
+const (
+	StatusPending    Status = "PENDING"
+	StatusInProgress Status = "IN_PROGRESS"
+	StatusCompleted  Status = "COMPLETED"
+	StatusFailed     Status = "FAILED"
+	StatusCancelled  Status = "CANCELLED"
+	StatusUnknown    Status = "UNKNOWN"
+	StatusRunning    Status = "RUNNING"
+)
+
+type Task struct {
+	ID         string          `json:"id"`
+	Type       string          `json:"type"`
+	Payload    json.RawMessage `json:"payload"`
+	Status     Status          `json:"status"`
+	Attempts   int             `json:"attempts"`
+	MaxRetries int             `json:"max_retries"`
+	CreatedAt  time.Time       `json:"created_at"`
+}
+
+func NewTask(id, taskType string, payload any) (*Task, error) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	t := &Task{
+		ID:         id,
+		Type:       taskType,
+		Payload:    data,
+		Status:     StatusPending,
+		Attempts:   0,
+		MaxRetries: 3,
+		CreatedAt:  time.Now(),
+	}
+
+	return t, nil
+}
